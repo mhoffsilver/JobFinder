a31000c1cff030bc2d4f1d2d7b2e38262b1c9622
mhoffsilver <morten@aersoe.dk>
2013-02-06 09:03:53 +0100

    Update containing some cleaning out in the DataWriter - Simplify FTW

--- src/dk/aersoe/jobfinder/crud/helpers/DataWriter.java
+++ src/dk/aersoe/jobfinder/crud/helpers/DataWriter.java
@@ -27,56 +27,38 @@ public class DataWriter {
 	 */
 	public boolean writeJobEntry(JobEntry entry, boolean modify) throws Throwable{
 		boolean result = false;
-		//Logger log = Logger.getRootLogger();
 		// TODO: Find the best suitable (with respect to the App Server) Logger 
 		Connection con = null;
 		try{
 			String dsName = "JobFinderResource";
 			DataSource ds = (javax.sql.DataSource)new InitialContext().lookup(dsName);
 			con = ds.getConnection();
+			String createStmt = "insert into JobEntries (creation_date, modify_date, foreign_date, status, deadline, title, company, description, url, source) values(?,?,?,?,?,?,?,?,?,?)";
 			if (modify){
-				// Update requested
-				String createStmt = "update JobEntries set creation_date=?, modify_date=?, foreign_date=?, status=?, deadline=?, title=?, company=?, description=?, url=?, source?) where id=?";
-				con.setAutoCommit(false);
-				PreparedStatement pstmt = con.prepareStatement(createStmt);
-				pstmt.setDate(1, new Date(entry.getCreationDate().getTime()));
-				pstmt.setDate(2, new Date(entry.getModifyDate().getTime()));
-				pstmt.setDate(3, new Date(entry.getForeignDate().getTime()));
-				pstmt.setInt(4, entry.getStatus());
-				pstmt.setDate(5, new Date(entry.getDeadline().getTime()));
-				pstmt.setString(6, entry.getTitle());
-				pstmt.setString(7, entry.getCompany());
-				pstmt.setString(8, entry.getDescription());
-				pstmt.setString(9,  entry.getUrl());
-				pstmt.setString(10, entry.getSource());
+				createStmt = "update JobEntries set creation_date=?, modify_date=?, foreign_date=?, status=?, deadline=?, title=?, company=?, description=?, url=?, source?) where id=?";
+			}
+			con.setAutoCommit(false);
+			PreparedStatement pstmt = con.prepareStatement(createStmt);
+			pstmt.setDate(1, new Date(entry.getCreationDate().getTime()));
+			pstmt.setDate(2, new Date(entry.getModifyDate().getTime()));
+			pstmt.setDate(3, new Date(entry.getForeignDate().getTime()));
+			pstmt.setInt(4, entry.getStatus());
+			pstmt.setDate(5, new Date(entry.getDeadline().getTime()));
+			pstmt.setString(6, entry.getTitle());
+			pstmt.setString(7, entry.getCompany());
+			pstmt.setString(8, entry.getDescription());
+			pstmt.setString(9,  entry.getUrl());
+			pstmt.setString(10, entry.getSource());
+			if (modify){
 				pstmt.setInt(11, entry.getId());
-				pstmt.executeUpdate();
-				con.commit();
-				result = true;
 			}
-			else{
-				// Create requested
-				String createStmt = "insert into JobEntries (creation_date, modify_date, foreign_date, status, deadline, title, company, description, url, source) values(?,?,?,?,?,?,?,?,?,?)";
-				con.setAutoCommit(false);
-				PreparedStatement pstmt = con.prepareStatement(createStmt);
-				pstmt.setDate(1, new Date(entry.getCreationDate().getTime()));
-				pstmt.setDate(2, new Date(entry.getModifyDate().getTime()));
-				pstmt.setDate(3, new Date(entry.getForeignDate().getTime()));
-				pstmt.setInt(4, entry.getStatus());
-				pstmt.setDate(5, new Date(entry.getDeadline().getTime()));
-				pstmt.setString(6, entry.getTitle());
-				pstmt.setString(7, entry.getCompany());
-				pstmt.setString(8, entry.getDescription());
-				pstmt.setString(9,  entry.getUrl());
-				pstmt.setString(10, entry.getSource());
-				pstmt.executeUpdate();
-				con.commit();
-				result = true;
-			}
+			pstmt.executeUpdate();
+			con.commit();
+			result = true;
 		}	
 		catch (Exception e){
 			throw e;
-			// TODO: Something intelligent should be said here
+			// TODO: Something intelligent should be logged here
 		}
 		finally{
 			if (con != null){
